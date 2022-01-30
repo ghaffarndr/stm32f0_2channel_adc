9da97f58236c895028b9d5211c5068e79d60ec20
ghaffarndr <ghaffarndr@gmail.com>
2022-01-30 21:41:50 +0330

    upload all files

---
+++ Objects/main.d
@@ -0,0 +1,9 @@
+./objects/main.o: main.c \
+  C:\Keil_v5\ARM\PACK\Keil\STM32F0xx_DFP\2.1.0\Drivers\CMSIS\Device\ST\STM32F0xx\Include\stm32f0xx.h \
+  C:\Keil_v5\ARM\PACK\Keil\STM32F0xx_DFP\2.1.0\Drivers\CMSIS\Device\ST\STM32F0xx\Include\stm32f030x6.h \
+  C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm0.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\PACK\Keil\STM32F0xx_DFP\2.1.0\Drivers\CMSIS\Device\ST\STM32F0xx\Include\system_stm32f0xx.h
---
+++ main.c
@@ -0,0 +1,45 @@
+// This program reads the analog voltage on PB4 and ...
+
+#include "stm32f0xx.h"                  // Device header
+
+static  uint32_t val1;
+static  uint32_t val2;
+
+static void delay(int dly)
+{
+  while( dly--);
+}
+ 
+static void initADC()
+{ 
+  RCC->AHBENR |= RCC_AHBENR_GPIOAEN;     					 // Turn on GPIOA
+  RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;              // Turn on ADC   
+  GPIOB->MODER |= (1<<10)|(1<<11)|(1<<12)|(1<<13); // Select analog mode for PB5/6   
+  ADC1->CR |= (1u<<31);                            // Begin ADCCalibration   
+  while ((ADC1->CR & (1u<<31)));                   // Wait for calibration complete:   
+  ADC->CCR |= (1<<22);	                           // Enable the reference voltage  
+  ADC1->CR |= (1<<0);                              // Enable the ADC 
+}
+
+int main()
+{
+	
+  initADC();
+  while(1) 
+		{
+		ADC1->CHSELR =0x00;
+    ADC1->CHSELR |= (1<<5);
+	  ADC1->CR |=  (1<<2);           // Trigger a conversion
+	  while ( (ADC1->CR & (1<<2)) ); // Wait for End of Conversion      	
+	  val1 = ADC1->DR;		         	 //Store the ADC conversion result
+		ADC1->CR |=  (1<<4);           // stop conversion
+    delay(100000);
+		ADC1->CHSELR =0x00;	
+  	ADC1->CHSELR |= (1<<6);
+	  ADC1->CR |=  (1<<2);           // Trigger a conversion
+	  while ( (ADC1->CR & (1<<2)) ); // Wait for End of Conversion 	
+	  val2 = ADC1->DR;               //Store the ADC conversion result
+		ADC1->CR |=  (1<<4);           // stop conversion 
+		delay(100000);
+  } 
+}
